9a023f309dfd962566ec22f008a90189256b9e6d
ahmed342k <165220891+ahmed342k@users.noreply.github.com>
2026-03-16 15:16:02 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMvjw8Pv_ixcecNYxsokBOAdjwCJvDnig",
@@ -11,7 +12,10 @@ const firebaseConfig = {
   measurementId: "G-79KCXRRXS2"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+await setPersistence(auth, browserLocalPersistence);
+
+export { app, db, auth };
